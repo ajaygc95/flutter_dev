a64652ffbfb2b85572434c19e0665b49b966fa2d
gcajay95 <gcajay95@gmail.com>
2022-09-12 01:13:04 -0700

    LockFin

---
+++ lockfin/android/app/src/main/kotlin/com/example/lockfin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lockfin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lockfin/lib/login_page.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 224, 224, 200),
+      body: SafeArea(
+        child: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Icon(
+              Icons.favorite,
+              size: 40,
+              color: Color.fromARGB(255, 230, 52, 141),
+            ),
+            // LOCKIN Name greeting AND logo
+            Text(
+              'LockFin',
+              style: GoogleFonts.bebasNeue(
+                  fontSize: 60.0, color: Colors.deepOrangeAccent),
+            ),
+            Text(
+              'Find your perfect match',
+              style: TextStyle(
+                color: Colors.purple,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white10),
+                    borderRadius: BorderRadius.circular(12)),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none, hintText: 'Email'),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white10),
+                    borderRadius: BorderRadius.circular(12)),
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: InputBorder.none, hintText: 'Password'),
+                  ),
+                ),
+              ),
+            ),
+            // email
+            // password
+            // Sign in Button
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      color: Colors.deepOrangeAccent,
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Center(
+                      child: Text(
+                    'Sign In',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17),
+                  ))),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+
+            // not a member? register now
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Not a member? "),
+                Text(
+                  'Register Now',
+                  style: TextStyle(
+                      color: Colors.lightBlue, fontWeight: FontWeight.bold),
+                )
+              ],
+            )
+          ]),
+        ),
+      ),
+    );
+  }
+}
